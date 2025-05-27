c7b3f7ba89d40599cd954047979fcc17d6ae0377
Tlevi_ <edulevixd@gmail.com>
2025-05-27 14:20:09 -0300

    mallocQ4 incompleto

---
+++ testes/mallocQ4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int tamanhoA, tamanhoB;
+
+	printf("Ditite o tamanho do vetor A e B: ");
+	scanf("%d %d", &tamanhoA, &tamanhoB);
+
+	int arrayA = (int *)malloc(sizeof(int) * tamanhoA);
+	printf("Digite os %d valores do array A: ", tamanhoA;
+	for(int i=0; i<tamanhoA; i++){
+		scanf("%d", &arrayA[i];
+	}
+	
+	int arrayB =
+	return 0;
+}
